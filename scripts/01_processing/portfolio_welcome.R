1c695dcf8f08cd308006feb9559daf8ca4dabac7
harrisonbowles <hxw831@miami.edu>
2026-08-28 10:46:24 -0400

    Forgot to save before previous commit

---
+++ scripts/01_processing/portfolio_welcome.R
@@ -0,0 +1,9 @@
+##Welcome to my portfolio! For now it is empty.
+##Feel free to run this for a message!
+
+
+string1 <- ("Hello JC and JD! Thank you for running this course, I can't wait to learn about R.")
+string2 <- ("JC, thank you for fixing my computer. It is still working!")
+string3 <- ("Have a great weekend!")
+
+x <- cat(string1,"\n",string2,"\n\n",string3)
